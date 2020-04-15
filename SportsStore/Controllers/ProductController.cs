da57d10a997703dd2d25e06af829294d671a0bdc
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-15 09:26:17 -0400

    configured views: _Layout, _ViewStart, and List

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private IProductRepository repository;
+        public ProductController(IProductRepository repo)
         {
-            return View();
+            repository = repo;
         }
+
+        public ViewResult List() => View(repository.Products);
     }
 }
