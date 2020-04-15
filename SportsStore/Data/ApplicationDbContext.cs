677beface4ffa19cfc189ed0885c3e969f756c7a
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-15 09:28:34 -0400

    added DbContext and installed EF

---
+++ SportsStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
