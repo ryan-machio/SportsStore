4525d4a4bac4f32020f13fe3a383a462b3e63cdb
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-17 00:30:47 -0400

    completed Chapter 10; redefined cart model w/ service, completed cart functionality, submitting orders functionality, and added validation to checkout and orders pages.

---
+++ SportsStore.Tests/OrderControlerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+namespace SportsStore.Tests
+{
+    public class OrderControllerTests
+    {
+        [Fact]
+        public void Cannot_Checkout_Empty_Cart() {
+            // Arrange - create a mock repository
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+            // Arrange - create an empty cart
+            Cart cart = new Cart();
+            // Arrange - create the order
+            Order order = new Order();
+            // Arrange - create an instance of the controller
+            OrderController target = new OrderController(mock.Object, cart);
+            // Act
+            ViewResult result = target.Checkout(order) as ViewResult;
+            // Assert - check that the order hasn't been stored
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+            // Assert - check that the method is returning the default view
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+            // Assert - check that I am passing an invalid model to the view
+            Assert.False(result.ViewData.ModelState.IsValid);
+        }
+    }
+}
