003bbdd80c3ac0d2f9671f8ddc5342994c730d81
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-20 00:29:44 -0400

    finalized Chapter 11: SportsStore Administration

---
+++ SportsStore/Migrations/20200420025816_ShippedOrders.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsStore.Migrations
+{
+    public partial class ShippedOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Shipped",
+                table: "Orders",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Shipped",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
